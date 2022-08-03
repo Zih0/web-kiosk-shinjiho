0d4dd9909cba099d81df7dc3672da31759cb7d09
Jiho Shin <ziho@kakao.com>
2022-08-03 17:02:33 +0900

    feat: 국제화 context provider 감싸기

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,17 @@ import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from 'src/styles/GlobalStyles'
 import { theme } from 'src/styles/theme'
+import Router from './Router'
+import InternationalizationProvider from 'src/contexts/InternationalizationContext'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-    </ThemeProvider>
+    <InternationalizationProvider>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </InternationalizationProvider>
   )
 }
 
