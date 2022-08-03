e74f242510c0730308b826d941ffbc5a423e3c44
Jiho Shin <ziho@kakao.com>
2022-08-03 22:22:23 +0900

    feat: 상품 API 구현

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,30 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { CategoriesModule } from './categories/categories.module'
+import { ProductsService } from './products/products.service'
+import { ProductsController } from './products/products.controller'
+import { ProductsModule } from './products/products.module'
+import { Category } from './categories/categories.entity'
+import { Product } from './products/products.entity'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [Category, Product],
+      synchronize: true,
+    }),
+    CategoriesModule,
+    ProductsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
