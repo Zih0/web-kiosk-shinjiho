5b0fa339216268d7ca9b5da781ec248b2d53b152
Jiho Shin <ziho@kakao.com>
2022-08-03 16:53:53 +0900

    feat: 라우터 컴포넌트 구현

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from 'src/styles/GlobalStyles'
 import { theme } from 'src/styles/theme'
+import Router from './Router'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+        <Router />
     </ThemeProvider>
   )
 }
