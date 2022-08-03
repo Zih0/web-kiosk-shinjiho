ad9e0f2d8b67bdb1f61e4eac4bffa5bf966c7f1f
Jiho Shin <ziho@kakao.com>
2022-08-03 17:50:40 +0900

    feat: PORT 환경변수로 관리

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,14 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  const app = await NestFactory.create(AppModule)
+  const configService = app.get(ConfigService)
+  const port = configService.get('PORT')
+
+  await app.listen(port)
+  Logger.log(`Application running on port ${port}`)
 }
-bootstrap();
+bootstrap()
