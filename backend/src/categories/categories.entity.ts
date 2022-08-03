edb74269aca817425ed01e97e1555960983efa53
Jiho Shin <ziho@kakao.com>
2022-08-03 22:03:19 +0900

    feat: 카테고리 엔티티 구현

---
+++ backend/src/categories/categories.entity.ts
@@ -0,0 +1,17 @@
+import { Product } from 'src/products/products.entity'
+import { BaseEntity, Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity()
+export class Category extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column()
+  kr_name: string
+
+  @Column()
+  en_name: string
+
+  @OneToMany((type) => Product, (product) => product.category, { eager: true })
+  products: Product[]
+}
