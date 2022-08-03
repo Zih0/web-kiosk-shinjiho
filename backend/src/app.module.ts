286b64c5f82e173d7adf22acbd107e4d45d24bd4
Jiho Shin <ziho@kakao.com>
2022-08-03 22:02:54 +0900

    feat: typeorm import

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { ProductsService } from './products/products.service'
+import { ProductsController } from './products/products.controller'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
   ],
-  controllers: [],
-  providers: [],
+  controllers: [ProductsController],
+  providers: [ProductsService],
 })
 export class AppModule {}
