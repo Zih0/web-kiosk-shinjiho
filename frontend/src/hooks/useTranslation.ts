c874cc57c4549ff6a2b4048499dae0b286bfe6e7
Jiho Shin <ziho@kakao.com>
2022-08-03 17:02:04 +0900

    feat: 언어 변경 useTranslation 훅 구현

---
+++ frontend/src/hooks/useTranslation.ts
@@ -0,0 +1,26 @@
+import { useContext, useLayoutEffect, useState } from 'react'
+import { InternationalizationContext } from 'src/contexts/InternationalizationContext'
+
+type TextObjType = Record<string, string>
+
+const useTranslation = (fileName: string) => {
+  const { language } = useContext(InternationalizationContext)
+  const [textObject, setTextObject] = useState<TextObjType>({})
+
+  const getJsonData = async () => {
+    const jsonData = await import(`src/i18n/${language}/${fileName}.json`)
+    setTextObject(jsonData)
+  }
+
+  useLayoutEffect(() => {
+    getJsonData()
+  }, [language])
+
+  const t = (key: string) => {
+    return textObject[key]
+  }
+
+  return t
+}
+
+export default useTranslation
