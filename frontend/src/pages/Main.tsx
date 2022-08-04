2a9d8b0742032e25dffedcbbf2e32eabade2bc59
Jiho Shin <ziho@kakao.com>
2022-08-05 00:09:24 +0900

    feat: 메인페이지에 카테고리 추가

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,9 +1,11 @@
 import Header from 'src/components/common/Header/Header'
+import CategoryTabs from 'src/components/MainPage/CategoryTabs'
 
 const Main = () => {
   return (
     <div>
       <Header />
+      <CategoryTabs />
     </div>
   )
 }
