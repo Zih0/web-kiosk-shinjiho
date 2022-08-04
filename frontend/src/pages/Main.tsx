f13c6b8c7d69da14e377726d27c8840bd84320a9
Jiho Shin <ziho@kakao.com>
2022-08-05 01:01:23 +0900

    feat: 메인 페이지 패딩 스타일링

--- frontend/src/pages/Main.tsx
+++ frontend/src/pages/Main.tsx
@@ -1,13 +1,18 @@
 import Header from 'src/components/common/Header/Header'
 import CategoryTabs from 'src/components/MainPage/CategoryTabs'
+import styled from 'styled-components'
 
 const Main = () => {
   return (
-    <div>
+    <Wrapper>
       <Header />
       <CategoryTabs />
-    </div>
+    </Wrapper>
   )
 }
 
 export default Main
+
+const Wrapper = styled.div`
+  padding: 0 42px;
+`
