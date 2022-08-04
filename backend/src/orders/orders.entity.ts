b71e9de95797a2556814c9a35c2bb8fa2737ffa8
Jiho Shin <ziho@kakao.com>
2022-08-04 21:43:36 +0900

    feat: order entity 작성

---
+++ backend/src/orders/orders.entity.ts
@@ -0,0 +1,25 @@
+import { OrderToProduct } from 'src/order-to-product/order-to-product.entity'
+
+import { BaseEntity, Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm'
+import { PaymentMethodEnum } from './orders.enum'
+
+@Entity()
+export class Order extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ type: 'enum', enum: PaymentMethodEnum })
+  payment_method: PaymentMethodEnum
+
+  @Column()
+  paid_amount: number
+
+  @Column()
+  total_amount: number
+
+  @CreateDateColumn()
+  paid_date: string
+
+  @OneToMany(() => OrderToProduct, (orderToProduct) => orderToProduct.order)
+  orderToProducts: OrderToProduct[]
+}
