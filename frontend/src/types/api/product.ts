f53840a1cf721a202aabbe9b78f99baf4bef6192
Jiho Shin <ziho@kakao.com>
2022-08-08 20:13:34 +0900

    feat: 상품 옵션 타입 추가

--- frontend/src/types/api/product.ts
+++ frontend/src/types/api/product.ts
@@ -1,10 +1,24 @@
+export interface ProductOptionDetailType {
+  id: number
+  kr_name: string
+  en_name: string
+  price: number
+}
+export interface ProductOptionType {
+  id: number
+  kr_name: string
+  en_name: string
+  is_required: boolean
+  option_details: ProductOptionDetailType[]
+}
+
 export interface ProductType {
   id: number
   kr_name: string
   en_name: string
   price: number
   thumbnail: string
-  option: Record<string, any[]>
+  options: ProductOptionType[]
   is_famous: boolean
   is_soldout: boolean
 }
