e1c6c908b9b4ca5e50ab74ddf74be2ca1d1d4504
Jiho Shin <ziho@kakao.com>
2022-08-09 13:23:44 +0900

    feat: count 커스텀 훅으로 분리

--- frontend/src/components/common/Modal/OptionSelectModal.tsx
+++ frontend/src/components/common/Modal/OptionSelectModal.tsx
@@ -1,6 +1,7 @@
-import React, { FC, useContext, useEffect, useState } from 'react'
+import React, { FC, useContext, useState } from 'react'
 import { useCartAction } from 'src/contexts/CartContext'
 import { InternationalizationContext } from 'src/contexts/InternationalizationContext'
+import useCount from 'src/hooks/useCount'
 import useTranslation from 'src/hooks/useTranslation'
 import { ProductOptionDetailType, ProductOptionType } from 'src/types/api/product'
 import { priceToString } from 'src/utils/priceUtil'
@@ -20,14 +21,11 @@ interface Props {
   options: ProductOptionType[]
 }
 
-const MIN_COUNT = 1
-const MAX_COUNT = 9
-
 const OptionSelectModal: FC<Props> = ({ open, onClose, id, imgUrl, krName, enName, price, options }) => {
   const { language } = useContext(InternationalizationContext)
   const { add } = useCartAction()
   const t = useTranslation('modal')
-  const [count, setCount] = useState(MIN_COUNT)
+  const { count, increaseCount, decreaseCount, isMaxCount, isMinCount } = useCount()
   const [extraPrice, setExtraPrice] = useState(0)
   const [selectedOption, setSelectedOption] = useState<Record<string, number | string>>({})
 
@@ -51,15 +49,11 @@ const OptionSelectModal: FC<Props> = ({ open, onClose, id, imgUrl, krName, enNam
   }
 
   const onClickMinus = () => {
-    if (count === MIN_COUNT) return
-
-    setCount((prev) => prev - 1)
+    decreaseCount()
   }
 
   const onClickPlus = () => {
-    if (count === MAX_COUNT) return
-
-    setCount((prev) => prev + 1)
+    increaseCount()
   }
 
   const onClickOptionDetail = (optionId: number, detail: ProductOptionDetailType) => {
@@ -126,14 +120,14 @@ const OptionSelectModal: FC<Props> = ({ open, onClose, id, imgUrl, krName, enNam
               name="iconCircleMinus"
               size={36}
               onClick={onClickMinus}
-              strokeColor={count === MIN_COUNT ? 'gray300' : 'black'}
+              strokeColor={isMinCount ? 'gray300' : 'black'}
             />
             <span>{count}</span>
             <Icon
               name="iconCirclePlus"
               size={36}
               onClick={onClickPlus}
-              strokeColor={count === MAX_COUNT ? 'gray300' : 'black'}
+              strokeColor={isMaxCount ? 'gray300' : 'black'}
             />
           </CountWrapper>
         </LeftSection>
