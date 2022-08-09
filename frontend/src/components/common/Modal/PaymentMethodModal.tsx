ed4b4335f1b814fe9ab101633c75732cbae05540
Jiho Shin <ziho@kakao.com>
2022-08-09 15:12:48 +0900

    feat: 결제수단 선택 모달 UI 구현

--- frontend/src/components/common/Modal/PaymentMethodModal.tsx
+++ frontend/src/components/common/Modal/PaymentMethodModal.tsx
@@ -1,9 +1,12 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
 
+import { useCartSummary } from 'src/contexts/CartContext'
 import useTranslation from 'src/hooks/useTranslation'
 
 import Modal from './Modal'
+
+import Icon from '../Icon/Icon'
 
 interface Props {
   open: boolean
@@ -12,9 +15,85 @@ interface Props {
 
 const PaymentMethodModal: FC<Props> = ({ open, onClose }) => {
   const t = useTranslation('modal')
-  return <Modal open={open} onClose={onClose} title={t('selectPaymentMethodTitle')}></Modal>
+  const { price } = useCartSummary()
+
+  const onClickCard = () => {}
+
+  const onClickCash = () => {}
+
+  return (
+    <Modal
+      open={open}
+      onClose={onClose}
+      title={t('selectPaymentMethodTitle')}
+      closeText={t('paymentCancelText')}
+      hasCloseButton
+    >
+      <Wrapper>
+        <TotalPrice>
+          결제금액 : <span className="price">{price}</span>
+        </TotalPrice>
+        <PaymentMethodWrapper>
+          <PaymentItem onClick={onClickCard}>
+            <Icon name="iconCreditCard" />
+            <PaymentName>{t('creditCard')}</PaymentName>
+          </PaymentItem>
+          <PaymentItem onClick={onClickCash}>
+            <Icon name="iconCash" />
+            <PaymentName>{t('cash')}</PaymentName>
+          </PaymentItem>
+        </PaymentMethodWrapper>
+      </Wrapper>
+    </Modal>
+  )
 }
 
 export default PaymentMethodModal
 
-const CartListWrapper = styled.div``
+const Wrapper = styled.div`
+  width: 100%;
+`
+
+const TotalPrice = styled.h3`
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 43px;
+
+  text-align: center;
+
+  ${({ theme }) => theme.color.black};
+
+  .price {
+    color: ${({ theme }) => theme.color.red};
+  }
+`
+
+const PaymentMethodWrapper = styled.div`
+  margin-top: 70px;
+  width: 100%;
+
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+`
+
+const PaymentItem = styled.button`
+  width: 160px;
+  height: 160px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 16px;
+
+  border: 1px solid ${({ theme }) => theme.color.black};
+  border-radius: 24px;
+`
+
+const PaymentName = styled.p`
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 29px;
+
+  color: ${({ theme }) => theme.color.black};
+`
