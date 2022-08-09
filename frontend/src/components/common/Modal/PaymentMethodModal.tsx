599f2b122af56d5e2dd356faaec04e0d57bfff38
Jiho Shin <ziho@kakao.com>
2022-08-09 14:44:12 +0900

    feat: 결제 수단 선택 제목 한/영

---
+++ frontend/src/components/common/Modal/PaymentMethodModal.tsx
@@ -0,0 +1,20 @@
+import React, { FC } from 'react'
+import styled from 'styled-components'
+
+import useTranslation from 'src/hooks/useTranslation'
+
+import Modal from './Modal'
+
+interface Props {
+  open: boolean
+  onClose: () => void
+}
+
+const PaymentMethodModal: FC<Props> = ({ open, onClose }) => {
+  const t = useTranslation('modal')
+  return <Modal open={open} onClose={onClose} title={t('checkCartTitle')}></Modal>
+}
+
+export default PaymentMethodModal
+
+const CartListWrapper = styled.div``
