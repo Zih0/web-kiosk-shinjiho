10f032cbb7abb7edb910f17d069413f5b551c47f
Jiho Shin <ziho@kakao.com>
2022-08-09 20:01:48 +0900

    feat: 주문 내역 모달 onSubmit 구현

--- frontend/src/components/common/Modal/OrderConfirmModal.tsx
+++ frontend/src/components/common/Modal/OrderConfirmModal.tsx
@@ -3,9 +3,11 @@ import styled from 'styled-components'
 
 import { useCartList } from 'src/contexts/CartContext'
 import { InternationalizationContext } from 'src/contexts/InternationalizationContext'
+import useModal from 'src/hooks/useModal'
 import useTranslation from 'src/hooks/useTranslation'
 
 import Modal from './Modal'
+import PaymentMethodModal from './PaymentMethodModal'
 
 import Icon from '../Icon/Icon'
 
@@ -18,35 +20,49 @@ const OrderConfirmModal: FC<Props> = ({ open, onClose }) => {
   const t = useTranslation('modal')
   const { language } = useContext(InternationalizationContext)
   const cartList = useCartList()
+  const {
+    open: openPaymentMethodModal,
+    onClose: onClosePaymentMethodModal,
+    onOpen: onOpenPaymentMethodModal,
+  } = useModal()
+
+  const onSubmit = () => {
+    onOpenPaymentMethodModal()
+    onClose()
+  }
 
   return (
-    <Modal
-      open={open}
-      onClose={onClose}
-      title={t('confirmOrderTitle')}
-      closeText={t('confirmOrderCancelText')}
-      submitText={t('confirmOrderSubmitText')}
-      hasCloseButton
-      hasSubmitButton
-    >
-      <Wrapper>
-        {cartList.map((cartItem) => (
-          <OrderItemWrapper key={cartItem.cartId}>
-            <OrderItemName>
-              {language === 'KR' && cartItem.kr_name}
-              {language === 'EN' && cartItem.en_name}
-            </OrderItemName>
-            <OrderItemCountWrapper>
-              <Icon name="iconCircleMinus" size={32} />
-              <p>{cartItem.count}</p>
-              <Icon name="iconCirclePlus" size={32} />
-            </OrderItemCountWrapper>
-            <Price>{cartItem.price * cartItem.count}</Price>
-            <Icon name="iconCircleXLine" size={32} />
-          </OrderItemWrapper>
-        ))}
-      </Wrapper>
-    </Modal>
+    <>
+      <Modal
+        open={open}
+        onClose={onClose}
+        onSubmit={onSubmit}
+        title={t('confirmOrderTitle')}
+        closeText={t('confirmOrderCancelText')}
+        submitText={t('confirmOrderSubmitText')}
+        hasCloseButton
+        hasSubmitButton
+      >
+        <Wrapper>
+          {cartList.map((cartItem) => (
+            <OrderItemWrapper key={cartItem.cartId}>
+              <OrderItemName>
+                {language === 'KR' && cartItem.kr_name}
+                {language === 'EN' && cartItem.en_name}
+              </OrderItemName>
+              <OrderItemCountWrapper>
+                <Icon name="iconCircleMinus" size={32} />
+                <p>{cartItem.count}</p>
+                <Icon name="iconCirclePlus" size={32} />
+              </OrderItemCountWrapper>
+              <Price>{cartItem.price * cartItem.count}</Price>
+              <Icon name="iconCircleXLine" size={32} />
+            </OrderItemWrapper>
+          ))}
+        </Wrapper>
+      </Modal>
+      <PaymentMethodModal open={openPaymentMethodModal} onClose={onClosePaymentMethodModal} />
+    </>
   )
 }
 
