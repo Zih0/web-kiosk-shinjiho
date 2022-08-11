fca821a66c0cb590dd73bee614b44d851d9ac193
Jiho Shin <ziho@kakao.com>
2022-08-11 16:06:10 +0900

    feat: Toast 구현

--- frontend/src/contexts/ToastContext.tsx
+++ frontend/src/contexts/ToastContext.tsx
@@ -1,13 +1,78 @@
-import { FC, createContext, useState } from 'react'
+import { FC, createContext, useContext, useEffect, useMemo, useRef, useState } from 'react'
 
-const ToastContext = createContext<Map<number, string>>(new Map())
+export type ToastStatus = 'success' | 'error'
+
+interface ToastType {
+  id: number
+  message: string
+  status: ToastStatus
+  timeout: number
+}
+
+interface ToastOptionType {
+  timeout?: number
+}
+
+const ToastContext = createContext<{ toastList: ToastType[]; removeToast: (id: number) => void }>({
+  toastList: [],
+  removeToast: () => {},
+})
+const ToastActionContext = createContext({
+  success: (message: string, options?: ToastOptionType) => {},
+  error: (message: string, options?: ToastOptionType) => {},
+})
 
 interface Props {
   children: React.ReactNode
   defaultTimeout?: number
 }
 
-const ToastProvider: FC<Props> = ({ children, defaultTimeout }) => {
-  const [toastMap, setToastMap] = useState<Map<number, string>>(new Map())
-  return <ToastContext.Provider value={toastMap}></ToastContext.Provider>
+const ToastProvider: FC<Props> = ({ children, defaultTimeout = 2000 }) => {
+  const [toastList, setToastList] = useState<ToastType[]>([])
+  const idRef = useRef(0)
+
+  const addToast = (message: string, status: ToastStatus, options?: ToastOptionType) => {
+    const toastId = idRef.current++
+
+    const newToast = {
+      id: toastId,
+      message,
+      status,
+      timeout: options?.timeout ?? defaultTimeout,
+    }
+
+    setToastList((prev) => [...prev, newToast])
+  }
+
+  const removeToast = (id: number) => {
+    setToastList((prev) => prev.filter((item) => item.id !== id))
+  }
+
+  const actions = useMemo(
+    () => ({
+      success: (message: string, options?: ToastOptionType) => addToast(message, 'success', options),
+      error: (message: string, options?: ToastOptionType) => addToast(message, 'error', options),
+    }),
+    [],
+  )
+
+  return (
+    <ToastActionContext.Provider value={actions}>
+      <ToastContext.Provider value={{ toastList, removeToast }}>{children}</ToastContext.Provider>
+    </ToastActionContext.Provider>
+  )
 }
+
+export const useToastFactory = () => {
+  const value = useContext(ToastContext)
+
+  return value
+}
+
+export const useToast = () => {
+  const toast = useContext(ToastActionContext)
+
+  return toast
+}
+
+export default ToastProvider
