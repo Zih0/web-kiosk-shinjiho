795bbc635fcc467dd024d9b3d924f411523bc637
Jiho Shin <ziho@kakao.com>
2022-08-11 13:34:02 +0900

    refactor: 무터치 시간 안내 국제화 처리

--- frontend/src/components/Cart/Time.tsx
+++ frontend/src/components/Cart/Time.tsx
@@ -2,11 +2,13 @@ import styled from 'styled-components'
 
 import { useCartAction } from 'src/contexts/CartContext'
 import useTimer from 'src/hooks/useTimer'
+import useTranslation from 'src/hooks/useTranslation'
 import { useRouter } from 'src/lib/router/Routes'
 
 const TIME_SHOW_TRIGGER = 10
 
 const Time = () => {
+  const t = useTranslation('main')
   const router = useRouter()
   const { clear } = useCartAction()
 
@@ -17,14 +19,14 @@ const Time = () => {
 
   const { time } = useTimer(reset)
 
-  return <TimeText>{time <= TIME_SHOW_TRIGGER && `${time}초 동안 아무런 반응이 없으면 자동으로 종료됩니다.`}</TimeText>
+  return <TimeText>{time <= TIME_SHOW_TRIGGER && `${t('disappearText1')} ${time} ${t('disappearText2')}`}</TimeText>
 }
 
 const TimeText = styled.p`
   margin-bottom: 12px;
   height: 24px;
 
-  font-size: 24px;
+  font-size: 30px;
 
   text-align: end;
 
